030c2e5d0f075be64ce986b281f29fcabf6887e0
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-03-12 22:59:54 -0300

    Colocando as feature

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -21,15 +21,15 @@ describe('E-commerce Saucedemo - Finalizar compra', () => {
         
         cy.get('[data-test="continue"]').click();
 
-        cy.get('.inventory_item_name').contains('Sauce Labs Fleece Jacket').should('be.visible')
+        cy.get('.inventory_item_name').contains('Sauce Labs Fleece Jacket').should('be.visible');
 
-        cy.get('[data-test="finish"]').click()
+        cy.get('[data-test="finish"]').click();
 
         cy.get('.complete-header').contains('Thank you for your order!').should('be.visible')
         cy.get('.complete-text').contains('Your order has been dispatched, and will arrive just as fast as the pony can get there!')
-        .should('be.visible')
+        .should('be.visible');
 
-        cy.get('[data-test="back-to-products"]').click()
+        cy.get('[data-test="back-to-products"]').click();
 
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,4 @@ Cypress.Commands.add('login', () => {
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
-})
-
-
+});
