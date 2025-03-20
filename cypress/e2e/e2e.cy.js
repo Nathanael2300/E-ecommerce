d45519196e1c207005d4645f2236522cce308176
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-03-19 22:11:03 -0300

    Update e2e.cy.js
    colocando as variáveis fora do describe!!!

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,4 +1,9 @@
 import { faker } from '@faker-js/faker';
+
+const firstName = faker.person.firstName(); 
+const lastName = faker.person.lastName();    
+const zipCode = faker.location.zipCode();
+
 describe('E-commerce Saucedemo - Finalizar compra', () => {
     beforeEach(() => {cy.visit('/');cy.login()});
     it('Deve realizar a compra de um produto', () => {
@@ -8,28 +13,15 @@ describe('E-commerce Saucedemo - Finalizar compra', () => {
         cy.get('[data-test="shopping-cart-link"]').click();
         cy.get('.inventory_item_name').contains('Sauce Labs Fleece Jacket').should('be.visible');
         cy.get('[data-test="checkout"]').click();
-        
-
-        const firstName = faker.person.firstName(); 
-        const lastName = faker.person.lastName();    
-        const zipCode = faker.location.zipCode();
-
-
         cy.get('[data-test="firstName"]').type(firstName);
         cy.get('[data-test="lastName"]').type(lastName);
         cy.get('[data-test="postalCode"]').type(zipCode);
-        
         cy.get('[data-test="continue"]').click();
-
         cy.get('.inventory_item_name').contains('Sauce Labs Fleece Jacket').should('be.visible');
-
         cy.get('[data-test="finish"]').click();
-
         cy.get('.complete-header').contains('Thank you for your order!').should('be.visible')
         cy.get('.complete-text').contains('Your order has been dispatched, and will arrive just as fast as the pony can get there!')
         .should('be.visible');
-
         cy.get('[data-test="back-to-products"]').click();
-
     });
 });
